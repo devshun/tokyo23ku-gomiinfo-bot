993c8959ebf56bf9ee48e56eb7932db5f875b690
shunya hayashi <hayashun0226@icloud.com>
2023-07-13 05:00:50 +0900

    fix migrate sql

--- infra/db/migrations/000001_create_garbage_collection_schedules.up.sql
+++ infra/db/migrations/000001_create_garbage_collection_schedules.up.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS garbage_collection_schedules (
-  id BIGSERIAL NOT NULL PRIMARY KEY,
+  id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   region VARCHAR(255),
   burnable_garbage_days VARCHAR(255),
   non_burnable_garbage_days VARCHAR(255),
-  recyclable_garbage_days VARCHAR(255)
-  created_at TIMESTAMP WITH TIME ZONE,
-  updated_at TIMESTAMP WITH TIME ZONE,
-  deleted_at TIMESTAMP WITH TIME ZONE,
+  recyclable_garbage_days VARCHAR(255),
+  created_at TIMESTAMP,
+  updated_at TIMESTAMP,
+  deleted_at TIMESTAMP
 );
