a8614e7c2bc4c1bda6b890a2a63541c7e393c0a1
shunya hayashi <hayashun0226@icloud.com>
2023-07-13 06:53:25 +0900

    add migrations

--- infra/db/migrations/000001_create_garbage_collection_schedules.down.sql
+++
@@ -1,1 +0,0 @@
-DROP TABLE IF EXISTS garbase_collection_schedules;
---
+++ infra/db/migrations/000003_garbage_days.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE garbage_days (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  region_id INT,
+  garbage_type VARCHAR(255) NOT NULL,
+  day_of_week VARCHAR(255) NOT NULL,
+  week_of_month INT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  FOREIGN KEY (region_id) REFERENCES regions(id)
+);
