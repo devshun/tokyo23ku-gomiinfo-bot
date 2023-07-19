bf9ac1af55cb3349c7f07893e76751ffd9d3c426
shunya hayashi <hayashun0226@icloud.com>
2023-07-20 05:15:52 +0900

    fix garbage column type

--- db/migrations/000003_garbage_days.up.sql
+++ db/migrations/000003_garbage_days.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE garbage_days (
   id INT AUTO_INCREMENT PRIMARY KEY,
   region_id INT,
-  garbage_type VARCHAR(255) NOT NULL,
+  garbage_type INT NOT NULL,
   day_of_week INT NOT NULL,
   week_number_of_month INT,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
