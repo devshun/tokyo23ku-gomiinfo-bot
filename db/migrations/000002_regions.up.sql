60ef7c1160b902e6800f1db5dabfbc9202f259f7
shunya hayashi <hayashun0226@icloud.com>
2023-07-20 06:29:00 +0900

    fix 文法エラー

--- db/migrations/000002_regions.up.sql
+++ db/migrations/000002_regions.up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE regions (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  ward_id INT,
+  ward_id INT NOT NULL,
   name VARCHAR(255) NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   FOREIGN KEY (ward_id) REFERENCES wards(id)
-  CONSTRAINT regions_unique UNIQUE (ward_id, name)
+  UNIQUE KEY uc_regions (ward_id, name)
 );
--- db/migrations/000003_garbage_days.up.sql
+++ db/migrations/000003_garbage_days.up.sql
@@ -1,11 +1,11 @@
 CREATE TABLE garbage_days (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  region_id INT,
+  region_id INT NOT NULL,
   garbage_type INT NOT NULL,
   day_of_week INT NOT NULL,
   week_number_of_month INT,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  FOREIGN KEY (region_id) REFERENCES regions(id)
-  CONSTRAINT garbage_days_unique UNIQUE (region_id, garbage_type, day_of_week, week_number_of_month)
+  FOREIGN KEY (region_id) REFERENCES regions(id),
+  UNIQUE KEY uc_garbage_days (region_id, garbage_type, day_of_week, week_number_of_month)
 );
