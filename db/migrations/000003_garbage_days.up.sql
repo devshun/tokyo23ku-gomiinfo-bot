4660b5b4a4d05d703364197bc688f71afded4b59
shunya hayashi <hayashun0226@icloud.com>
2023-07-20 04:01:20 +0900

    fix table schema

--- db/migrations/000003_garbage_days.up.sql
+++ db/migrations/000003_garbage_days.up.sql
@@ -2,8 +2,8 @@ CREATE TABLE garbage_days (
   id INT AUTO_INCREMENT PRIMARY KEY,
   region_id INT,
   garbage_type VARCHAR(255) NOT NULL,
-  day_of_week VARCHAR(255) NOT NULL,
-  week_of_month INT,
+  day_of_week INT NOT NULL,
+  week_number_of_month INT,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   FOREIGN KEY (region_id) REFERENCES regions(id)
