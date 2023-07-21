bc5c02b130853d38c2eb6c328c3e24791c694d8b
shunya hayashi <hayashun0226@icloud.com>
2023-07-21 13:03:21 +0900

    delete healthからdb削除

--- app/handler/health/main.go
+++ app/handler/health/main.go
@@ -3,17 +3,9 @@ package main
 import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	db "github.com/devshun/tokyo23ku-gomiinfo-bot/infrastructure"
 )
 
 func handleRequest() (events.APIGatewayProxyResponse, error) {
-
-	_, err := db.Init()
-
-	if err != nil {
-		panic(err)
-	}
-
 	return events.APIGatewayProxyResponse{
 		Body:       "OK",
 		StatusCode: 200,
