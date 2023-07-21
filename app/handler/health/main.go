7f7abf07ba17636d07ab570c8a4b8e697bc4f400
shunya hayashi <hayashun0226@icloud.com>
2023-07-21 13:27:03 +0900

    fix helthのレスポンスでerrorを返す

--- app/handler/health/main.go
+++ app/handler/health/main.go
@@ -3,9 +3,20 @@ package main
 import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	db "github.com/devshun/tokyo23ku-gomiinfo-bot/infrastructure"
 )
 
 func handleRequest() (events.APIGatewayProxyResponse, error) {
+
+	_, err := db.Init()
+
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Body:       err.Error(),
+		}, nil
+	}
+
 	return events.APIGatewayProxyResponse{
 		Body:       "OK",
 		StatusCode: 200,
