3c9015f72c2673a72718d31b2b470bee3d3ccbe2
shunya hayashi <hayashun0226@icloud.com>
2023-07-22 19:34:04 +0900

    add regionにblock numberカラムを追加

--- app/domain/model/region.go
+++ app/domain/model/region.go
@@ -6,6 +6,7 @@ type Region struct {
 	ID          int          `gorm:"primaryKey;autoIncrement" json:"id,omitempty"`
 	WardID      int          `gorm:"not null" json:"wardId,omitempty"`
 	Name        string       `gorm:"size:255;not null" json:"name,omitempty"`
+	BlockNumber *int         `gorm:"block_number" json:"blockNumber,omitempty"`
 	CreatedAt   time.Time    `gorm:"autoCreateTime" json:"createdAt,omitempty"`
 	UpdatedAt   time.Time    `gorm:"autoUpdateTime" json:"updatedAt,omitempty"`
 	Ward        Ward         `gorm:"foreignKey:WardID" json:"ward,omitempty"`
