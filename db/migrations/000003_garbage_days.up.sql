e25cb2d24c1f91c04e997603b442ce7a1c5b23df
shunya hayashi <hayashun0226@icloud.com>
2023-07-23 10:06:51 +0900

    add 制約を追加

--- db/migrations/000003_garbage_days.up.sql
+++ db/migrations/000003_garbage_days.up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE garbage_days (
   id INT AUTO_INCREMENT PRIMARY KEY,
   region_id INT NOT NULL,
-  garbage_type INT NOT NULL,
-  day_of_week INT NOT NULL,
-  week_number_of_month INT,
+  garbage_type INT NOT NULL CHECK (garbage_type BETWEEN 1 AND 3),
+  day_of_week INT NOT NULL CHECK (day_of_week BETWEEN 1 AND 7),
+  week_number_of_month INT CHECK (week_number_of_month BETWEEN 0 AND 4),
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   FOREIGN KEY (region_id) REFERENCES regions(id),
